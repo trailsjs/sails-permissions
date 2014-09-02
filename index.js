1bb03dfebfe1ca05c77b6addcecb0b15616e3065
Travis Webb <me@traviswebb.com>
2014-09-02 16:00:14 -0400

    moved modules to api/models

--- index.js
+++ index.js
@@ -5,14 +5,10 @@ var injector = require('sails-inject-models');
 module.exports = function (sails) {
   return {
     initialize: function (next) {
-      sails.log.verbose('sails-permissions: injecting models');
-
-      injector.inject(sails, [
-        { name: 'Role',       module: require('./lib/models/Role') },
-        { name: 'Permission', module: require('./lib/models/Permission') },
-        { name: 'User',       module: require('./lib/models/User') },
-        { name: 'Model',      module: require('./lib/models/Model') }
-      ], next);
+      injector.injectApp({
+        sails: sails,
+        module: module.id
+      }, next);
     }
   };
 };
