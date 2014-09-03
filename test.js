158491d40d67624f2d091e56f3613716bb9ab2e1
Travis Webb <me@traviswebb.com>
2014-09-03 15:39:28 -0400

    stubbed unit tests

--- test.js
+++ test.js
@@ -1,5 +1,20 @@
+'use strict';
+
+var assert = require('assert');
+var rigger = require('sails-rigged');
+
 describe('sails-permissions', function () {
   var sp = require('./');
+  var sails;
+
+  before(function (done) {
+    this.timeout(10000);
+    rigger.lift('xtuple-api', function (_sails) {
+      sails = _sails;
+      done();
+    });
+
+  });
 
   describe('Permission', function () {
     describe('#permits()', function () {
@@ -7,21 +22,27 @@ describe('sails-permissions', function () {
         var permission;
         before(function (done) {
           permission = Permission.create({
+
           });
+          done();
         });
         it('should return the correct owner if the action is permitted', function (done) {
 
+          done();
         });
         it('should return null if no ownership relation permits the action', function (done) {
 
+          done();
         });
       });
       describe('@static', function () {
         it('should return the correct owner if the action is permitted', function (done) {
 
+          done();
         });
         it('should return null if no ownership relation permits the action', function (done) {
 
+          done();
         });
       });
 
