e31028bb4b5b36c47e0641f82c6451a6408b6b95
Travis Webb <me@traviswebb.com>
2014-11-11 02:34:11 -0500

    disallow users querying other users

--- api/policies/OwnerPolicy.js
+++ api/policies/OwnerPolicy.js
@@ -4,15 +4,23 @@
 module.exports = function OwnerPolicy (req, res, next) {
   if (!req.user || !req.user.id) return next(new Error('req.user is not set'));
 
+  sails.log(req.query);
+  sails.log(req.params);
+
   if (req.options.modelName === 'user') {
     req.body = req.body || { };
     req.body.id = req.user.id;
     req.query.id = req.user.id;
+
+    if (!_.isEmpty(req.params.id) && req.params.id != req.user.id) {
+      return res.send(400, 'you cannot query another user');
+    }
   }
   else if (!req.options.ignoreOwnership) {
     req.body = req.body || { };
     req.body.owner = req.user.id;
     req.query.owner = req.user.id;
+    req.params.owner = req.user.id;
   }
 
   next();
