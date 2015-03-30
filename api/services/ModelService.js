9020dd76a74f3d1e203012051745d195c13de662
Travis Webb <me@traviswebb.com>
2015-03-30 04:26:54 -0400

    added TODO note

--- api/services/ModelService.js
+++ api/services/ModelService.js
@@ -5,6 +5,8 @@ module.exports = {
    * Return the type of model acted upon by this request.
    */
   getTargetModelName: function (req) {
+    // TODO there has to be a more sails-y way to do this without including
+    // external modules
     if (_.isString(req.options.alias)) {
       sails.log.silly('singularizing', req.options.alias, 'to use as target model');
       return pluralize.singular(req.options.alias);
