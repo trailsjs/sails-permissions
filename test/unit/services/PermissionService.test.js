25c326eab4d9b233540087d22b8602330a79e897
Brian Gulotta <brian.gulotta@gmail.com>
2015-04-02 12:02:28 -0500

    adding unit tests for current permissionservice functionality. still have 2 methods left to cover.

--- test/unit/services/PermissionService.test.js
+++ test/unit/services/PermissionService.test.js
@@ -9,4 +9,108 @@ describe('Permission Service', function () {
 
   });
 
+  describe('#isForeignObject()', function () {
+
+    it('should return true if object is not owned by the requesting user', function (done) {
+
+      var objectNotOwnedByUser = { owner: 2 };
+      var user = 1;
+
+      assert.equal(sails.services.permissionservice.isForeignObject(user)(objectNotOwnedByUser), true);
+
+      done();
+
+    });
+
+    it('should return false if object is owned by the requesting user', function (done) {
+
+      var objectOwnedByUser = { owner: 1 };
+      var user = 1;
+
+      assert.equal(sails.services.permissionservice.isForeignObject(user)(objectOwnedByUser), false);
+
+      done();
+    });
+
+  });
+
+  describe('#hasForeignObjects()', function () {
+
+    it('should return true if any object is not owned by the requesting user', function (done) {
+
+      var objectOwnedByUser = { owner: 1 };
+      var objectNotOwnedByUser = { owner: 2 };
+      var user = { id: 1 };
+
+      assert.equal(sails.services.permissionservice.hasForeignObjects([ objectNotOwnedByUser, objectOwnedByUser ], user), true);
+
+      done();
+    });
+
+    it('should return false if all objects are owned by the requesting user', function (done) {
+
+      var objectOwnedByUser = { owner: 1 };
+      var objectOwnedByUser2 = { owner: 1 };
+      var user = { id: 1 };
+
+      assert.equal(sails.services.permissionservice.hasForeignObjects([ objectOwnedByUser2, objectOwnedByUser ], user), false);
+      done();
+
+    });
+
+  });
+
+  describe('#hasOwnershipPolicy()', function () {
+
+    it('should return true if object supports ownership policy', function (done) {
+
+      assert.equal(sails.services.permissionservice.hasOwnershipPolicy({ autoCreatedBy: true }), true);
+      done();
+    });
+
+    it('should return false if object does not support ownership policy', function (done) {
+
+      assert.equal(sails.services.permissionservice.hasOwnershipPolicy({ autoCreatedBy: false }), false);
+      done();
+
+    });
+
+  });
+
+  describe('#getMethod()', function () {
+
+    it ('should return \'create\' if POST request', function(done) {
+
+      assert.equal(sails.services.permissionservice.getMethod('POST'), 'create');
+      done();
+
+    });
+
+    it ('should return \'update\' if PUT request', function(done) {
+
+      assert.equal(sails.services.permissionservice.getMethod('PUT'), 'update');
+      done();
+
+    });
+
+    it ('should return \'read\' if GET request', function(done) {
+
+      assert.equal(sails.services.permissionservice.getMethod('GET'), 'read');
+      done();
+
+    });
+
+    it ('should return \'delete\' if DELETE request', function(done) {
+
+      assert.equal(sails.services.permissionservice.getMethod('DELETE'), 'delete');
+      done();
+
+    });
+
+  });
+
+  //TODO: add unit tests for #findTargetObjects()
+
+  //TODO: add unit tests for #findModelPermissions()
+
 });
