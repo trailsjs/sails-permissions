f1110308ba08f42f51fd29fd726aee4c8598adc7
Travis Webb <me@traviswebb.com>
2015-07-17 23:15:22 -0400

    cleanup OwnerPolicy

--- api/policies/OwnerPolicy.js
+++ api/policies/OwnerPolicy.js
@@ -1,20 +1,13 @@
 /**
- * TODO - this is setting createdBy, not owner.
- * The comment below, and the name of this file/function is confusing to me
  * Ensure that the 'owner' property of an Object is set upon creation.
+ *
+ * TODO combine with RolePolicy in 2.0
  */
 module.exports = function OwnerPolicy (req, res, next) {
-  //sails.log('OwnerPolicy()');
   if (!req.user || !req.user.id) {
     req.logout();
     return res.send(500, new Error('req.user is not set'));
   }
-
-  /*
-  sails.log('OwnerPolicy user', req.user);
-  sails.log('OwnerPolicy method', req.method);
-  sails.log('OwnerPolicy req.body', req.body);
-  */
 
   if (req.model.autoCreatedBy === false) {
     // sails.log('OwnerPolicy hasOwnershipPolicy: false');
@@ -22,10 +15,8 @@ module.exports = function OwnerPolicy (req, res, next) {
   }
 
   if ('POST' == req.method) {
-    //req.body || (req.body = { });
     req.body.createdBy = req.user.id;
   }
 
-  //sails.log('OwnerPolicy req.model', req.model);
   next();
 };
