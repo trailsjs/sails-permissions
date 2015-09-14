83a9c2b9bdef538bcd6de244aebec9314cada265
Max Stahl <mcodev1@gmail.com>
2015-09-14 14:44:09 -0400

    Use `model.name` instead of `model.globalId`

--- api/services/PermissionService.js
+++ api/services/PermissionService.js
@@ -177,7 +177,7 @@ module.exports = {
    */
   getErrorMessage: function(options) {
     return [
-      'User', options.user.email, 'is not permitted to', options.method, options.model.globalId
+      'User', options.user.email, 'is not permitted to', options.method, options.model.name
     ].join(' ');
   },
 
