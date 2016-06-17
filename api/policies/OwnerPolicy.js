488928f1f6c6daca22bd3d08b916bbf00b6dfbd9
Cristian Torres <cris.torres15@gmail.com>
2016-06-17 11:06:30 -0600

    FIX: Cannot read property 'autoCreatedBy' of undefined

--- api/policies/OwnerPolicy.js
+++ api/policies/OwnerPolicy.js
@@ -16,7 +16,7 @@ module.exports = function OwnerPolicy (req, res, next) {
   sails.log.verbose('OwnerPolicy req.body', req.body);
   */
 
-  if (req.options.modelDefinition.autoCreatedBy === false) {
+  if (req.options.modelDefinition !== undefined && req.options.modelDefinition.autoCreatedBy === false) {
     // sails.log.verbose('OwnerPolicy hasOwnershipPolicy: false');
     return next();
   }
